cf596f2d50f8d713a2a40c16eb35256ef3ed4437
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-26 15:02:10 +0200

    ts

--- 2/basics-03-core-data-types/app.ts
+++ 2/basics-03-core-data-types/app.ts
@@ -1,7 +1,7 @@
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect input!');
-  // }
+  if (typeof n1 !== "number" || typeof n2 !== "number") {
+    throw new Error("Incorrect input!");
+  }
   const result = n1 + n2;
   if (showResult) {
     console.log(phrase + result);
@@ -13,6 +13,6 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 const number1 = 5; // 5.0
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: ';
+const resultPhrase = "Result is: ";
 
 add(number1, number2, printResult, resultPhrase);
