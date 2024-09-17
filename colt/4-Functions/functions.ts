5efb6c302bcbfa1ddb6509ffe8d78beb17fa82fe
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-17 13:04:25 +0200

    function

---
+++ colt/4-Functions/functions.ts
@@ -0,0 +1,17 @@
+function square(num1: number) {
+  return num1 * num1;
+}
+
+square(2);
+
+function greeting(person: string) {
+  return `Hello ${person}`;
+}
+
+greeting("Larissa");
+
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
+doSomething("Larissa", 32, true);
+
+//For function you can definde the type of parameter
