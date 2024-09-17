a5bc7d85ebde2188d08f916849544bb3e70c98a9
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-17 12:52:57 +0200

    initialization

--- colt/3.11-Type-Annotation-Basics/any.ts
+++ colt/3.11-Type-Annotation-Basics/any.ts
@@ -4,3 +4,12 @@ let thing: any = "Hello";
 
 thing = 0;
 thing.toStringfy;
+
+const movies = ["Mamma Mia", "Evita", "The Greatest Showman"];
+let foundMOvie: string;
+
+for (let movie of movies) {
+  if (movie === "Mamma Mia") {
+    foundMOvie = "Mamma Mia";
+  }
+}
