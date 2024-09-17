4baa6f5582189c2a8b6ba3d2fbe1e291682f338b
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-17 11:08:09 +0200

    Defining a variable type

---
+++ colt/3.11-Type-Annotation-Basics/strings.ts
@@ -0,0 +1,11 @@
+// How to write a variable in TypeScript: const/let name : type = value;
+
+const newVariable: string = "Hello, There";
+
+// String type
+
+let movieTtile: string = "Titanic";
+
+movieTtile = "Mamma Mia";
+
+// As soon I define what Kind of type the variable it is, I can not by mistake assign another type of value than the same type of value. I also can not user methods which are apply for numbers and boleans, for intanse, Mathround (number) oder inclues (bolean). TS will show me only methods apply for my defined kind of type, in this case: string:
