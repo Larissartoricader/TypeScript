b2aac1d33c257d01068913fec38db185762686a2
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-20 10:28:41 +0200

    function definition

--- colt/4-Functions/functions.ts
+++ colt/4-Functions/functions.ts
@@ -13,3 +13,24 @@ const doSomething = (person: string, age: number, isFunny: boolean) => {};
 doSomething("Larissa", 32, true);
 
 //For function you can definde the type of parameter
+
+function getMeANumber(): number {
+  return Math.random();
+}
+
+//Or a function can return different kind os returns (ex. number OR string)
+
+function random(): string | number {
+  const randomNumber = Math.random();
+  if (randomNumber >= 0.5) {
+    return randomNumber.toString();
+  } else {
+    return randomNumber;
+  }
+}
+
+// it works also for anonymous function
+
+const add = (x: number, y: number): number => {
+  return x + y;
+};
