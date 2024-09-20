bb0b3501209742c7a710d400ee37e09530433dcd
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-20 14:59:48 +0200

    more objects in function

--- colt/5-Objects/objects.js
+++ colt/5-Objects/objects.js
@@ -3,13 +3,27 @@ var onePerson = {
     lastName: "Sartori",
     age: 37,
 };
-// How to define a paramete as object
+// How to define a parameter as object
 function newPrintName(person) {
-    console.log({ person: person });
+    console.log(person);
 }
 newPrintName(onePerson);
-// I can also dfine
+// I can also define in the function, what kind of key-properties-types have the Object, in This case, I only able to use the keyproperty that I declare in the function. I can not extende to other property, because I didnt mention them inside the function. So if I want to also log Age, It will no be possible, because only Name and LastName were declare as part of the object.
 function printName(person) {
     console.log("".concat(person.name, " ").concat(person.lastName));
 }
+printName({ name: "Um Novo Nome", lastName: "Um Novo Sobrenome" });
+//But if write the object as variable and write which key-property of this variable I need for my function. In this case, the object variable can have more key-properties and the function will only look for the ones that it needs to run.
 printName(onePerson);
+// By Writing the a variable that IS an object it is possible to specify not only the type of this variable, but also the types of the key property values inside:
+var dog = {
+    name: "Lulu",
+    age: 2,
+    isAngry: false,
+};
+// What happens if a function should return an OBJECT? It is possible to define the function return like void, number, string OR {} and it means an object:
+var coordinate = { x: 584, y: 5842 };
+function randomCoordinate() {
+    return { x: Math.random(), y: Math.random() };
+}
+console.log(randomCoordinate());
--- colt/5-Objects/objects.ts
+++ colt/5-Objects/objects.ts
@@ -3,18 +3,40 @@ const onePerson = {
   lastName: "Sartori",
   age: 37,
 };
-// How to define a paramete as object
 
-function newPrintName(person: object) {
+// How to define a parameter as object
+
+function newPrintName(person: object): void {
   console.log(person);
 }
 
 newPrintName(onePerson);
 
-// I can also dfine
+// I can also define in the function, what kind of key-properties-types have the Object, in This case, I only able to use the keyproperty that I declare in the function. I can not extende to other property, because I didnt mention them inside the function. So if I want to also log Age, It will no be possible, because only Name and LastName were declare as part of the object.
 
 function printName(person: { name: string; lastName: string }) {
   console.log(`${person.name} ${person.lastName}`);
 }
 
+printName({ name: "Um Novo Nome", lastName: "Um Novo Sobrenome" });
+
+//But if write the object as variable and write which key-property of this variable I need for my function. In this case, the object variable can have more key-properties and the function will only look for the ones that it needs to run.
 printName(onePerson);
+
+// By Writing the a variable that IS an object it is possible to specify not only the type of this variable, but also the types of the key property values inside:
+
+let dog: { name: string; age: number; isAngry: boolean } = {
+  name: "Lulu",
+  age: 2,
+  isAngry: false,
+};
+
+// What happens if a function should return an OBJECT? It is possible to define the function return like void, number, string OR {} and it means an object:
+
+const coordinate: { x: number; y: number } = { x: 584, y: 5842 };
+
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
+
+console.log(randomCoordinate());
