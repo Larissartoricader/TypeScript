5ded53084249daa69059f9864c463c4176c634bc
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-27 12:13:38 +0200

    alias

---
+++ colt/5-Objects/alias.js
@@ -0,0 +1,9 @@
+// Alias is the definition of types for objects before we even write the values of the objects. It is like a definition, how th object should be declare when it happens.
+// Now I can user Person a TYPE for my code. I do not need to specify every single type of key, because this information is already declared inside the new created type Person, an Alias Type of TS.
+var larissa = {
+    name: "Larissa",
+    age: 37,
+    isHappy: false,
+    adresse: "Fruchtalle, 3",
+};
+//Person becomes my own default for future objects.
