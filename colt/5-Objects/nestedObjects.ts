8f8bb03fe3347c07c79c389ad0dc97cba141bf51
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-09-27 12:12:54 +0200

    alias

---
+++ colt/5-Objects/nestedObjects.ts
@@ -0,0 +1,37 @@
+//If you want to declare the type of values from an object inside a function as parameter, you can do it. But if you have nested object, it will become really complicate insert {} inisde {}. That is the point where alias is really helpful as nested objects.
+
+const bestSong = {
+  artist: "Anitta",
+  songName: "Bang Bang",
+  numStream: 32453,
+  credits: {
+    producer: "X Producer",
+    dj: "X DJ",
+    compositor: "X Compositor",
+  },
+  position: 2,
+};
+
+// That is how TS insider a function looks like.
+
+function printBestSound(bestSong: {
+  artist: string;
+  songName: string;
+  numStream: number;
+  credits: { producer: string; dj: string; compositor: string };
+  position: number;
+}) {
+  console.log(bestSong);
+}
+
+printBestSound(bestSong);
+
+// To avoid all specification inside a function, we can create an alias type and define it outside the function.
+
+type Song = {
+  artist: string;
+  songName: string;
+  numStream: number;
+  credits: { producer: string; dj: string; compositor: string };
+  position: number;
+};
