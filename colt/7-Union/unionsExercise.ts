4632b35db52bb7208e32c0a16f7dd3fdbef15aa6
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-10-02 14:33:28 +0200

    exercise

--- colt/7-Union/unionsExercise.ts
+++ colt/7-Union/unionsExercise.ts
@@ -2,6 +2,7 @@
 // ******************* PART 1 *******************
 // **********************************************
 // Create a variable called highScore that can be a number OR a boolean
+const highScore: number | boolean = false;
 
 // **********************************************
 // ******************* PART 2 *******************
@@ -9,13 +10,14 @@
 // create an array called stuff
 // it can be an array of numbers OR an array of strings
 // it cannot be an array of numbers and strings (mixed together)
+const stuff: number[] | string[] = ["hello", "hallo"];
 
 // **********************************************
 // ******************* PART 3 *******************
 // **********************************************
 // Create a literal type called SkillLevel
 // There are 4 allowed values: "Beginner", "Intermediate", "Advanced", and "Expert"
-
+type SkillLevel = "Beginner" | "Intermediate" | "Advanced" | "Expert";
 // **********************************************
 // ******************* PART 4 *******************
 // **********************************************
@@ -25,6 +27,13 @@
 // sport must be "ski" or "snowboard"
 // level must be a value from the SkillLevel type (from above)
 
+type SkiSchoolStudent = {
+  name: string;
+  age: number;
+  sport: "ski" | "snowboard";
+  level: SkillLevel;
+};
+
 // **********************************************
 // ******************* PART 5 *******************
 // **********************************************
@@ -33,15 +42,37 @@
 // g should be a number
 // b should be a number
 
+type RGB = {
+  r: number;
+  g: number;
+  b: number;
+};
+
 // Define a type to represent an HSL color
 // h should be a number
 // s should be a number
 // l should be a number
 
+type HSL = {
+  h: number;
+  s: number;
+  l: number;
+};
 // Create an array called colors that can hold a mixture of RGB and HSL color types
+
+const colors: (RGB | HSL)[] = [];
 
 // **********************************************
 // ******************* PART 6 *******************
 // **********************************************
 // Write a function called greet that accepts a single string OR an array of strings
 // It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+function greet(name: string | string[]): void {
+  console.log(`Hello, ${name}!`);
+}
+
+const listOfNames = ["Maria", "Larissa", "Anna", "Ana", "Claudia"];
+const onlyOneName = "Luana";
+
+greet(listOfNames);
+greet(onlyOneName);
