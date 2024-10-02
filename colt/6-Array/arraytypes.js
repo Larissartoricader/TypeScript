18b9feebb680ac602fdccbe922e664a04afac4b5
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-10-02 11:34:05 +0200

    array tpyes

--- colt/6-Array/arraytypes.js
+++ colt/6-Array/arraytypes.js
@@ -1,17 +1,11 @@
 // If I only define a variable with [] it means that the variable has to BE an EMPTY array. It is not possible to insert any values inside this array, because it has to be an empty array.
-
-const money: [] = [];
-
+var money = [];
 // Empty array does not allow a new value.
 // money.push = 2;
-
 // The right way to define a variable as an array it is to provide [] and define the type of values inside the array. Here are exemples of one type arrays:
-
-const salary: number[] = [2344.23, 34523.23, 3452, 34, 3455.34];
-
+var salary = [2344.23, 34523.23, 3452, 34, 3455.34];
 salary.push(1234.56);
 console.log(salary);
-const pets: string[] = ["peixe", "hailing", "bob"];
-
+var pets = ["peixe", "hailing", "bob"];
 pets.push("tartatura");
 console.log(pets);
